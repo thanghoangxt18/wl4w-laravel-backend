9edc8efea509939ea15893ec62c548041f076a12
Viki <thanghoangxt18@gmail.com>
2020-08-22 23:03:56 +0700

    update api update-order-exercise

--- app/Http/Resources/Exercise/ExerciseResource.php
+++ app/Http/Resources/Exercise/ExerciseResource.php
@@ -22,7 +22,7 @@ class ExerciseResource extends JsonResource
             'reps' => $this->format($this->reps, 'integer'),
             'time_per_rep' => $this->format($this->time_per_rep, 'integer'),
             'tts_guide' => $this->format($this->tts_guide),
-            'met' => $this->format($this->met, 'float'),
+            'met' => $this->format($this->met, 'double'),
         ];
     }
 }
