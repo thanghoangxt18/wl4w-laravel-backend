9aad25d492abc615600ad851bca36db6fbae9805
Quan Tran <quantt@vieted.com>
2020-09-24 18:37:26 +0700

    [Cors] Enable Cors always

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,9 @@
 <?php
-
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'allow_origins' => ['*'],
+    'allow_headers' => ['*'],
+    'allow_methods' => ['*'],
+    'allow_credentials' => false,
+    'expose_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
 ];
