457c80350f6a38417721c80527b2d1184982d661
Quan Tran <quanuet94@gmail.com>
2020-10-04 22:54:38 +0700

    [Music API] CRUD

---
+++ database/migrations/2020_10_04_104440_create_music_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMusicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->bigIncrements('id')->index();
+            $table->string('name')->nullable();
+            $table->string('url')->nullable();
+            $table->integer('bpm')->nullable();
+            $table->string('tag')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musics');
+    }
+}
